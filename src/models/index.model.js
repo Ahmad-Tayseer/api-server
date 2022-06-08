56e4894f1a7a84fb814ed5f21531e65e4927df51
AhmadTayseer <ahmadtayseer@ymail.com>
2022-06-08 22:15:40 +0300

    add dev branch

--- src/models/index.model.js
+++ src/models/index.model.js
@@ -9,15 +9,17 @@ const Collection = require('./collection.model');
 
 const POSTGRES_URI = process.env.NODE_ENV === "test" ? 'sqlite:memory:' : process.env.DATABASE_URL;
 
-const SequelizeOptions = process.env.NODE_ENV === "production" ? 
-{
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false,
+const SequelizeOptions = 
+    process.env.NODE_ENV === "production" 
+    ? 
+    {
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
         },
-    },
-} : {};
+    } : {};
 
 const sequelize = new Sequelize(POSTGRES_URI, SequelizeOptions);
 
